b56c026eba7aff4f6114342b4321cdc563ec9a42
huangjie0 <1364045672@qq.com>
2025-06-13 13:53:44 +0800

    让数字滚动起来

--- src/components/NumberCard.vue
+++ src/components/NumberCard.vue
@@ -1,16 +1,52 @@
 <template>
-    <div class="number-card data-center">
-        {{ value }}
+    <div class="number-card data-center" :class="'num-' + id">
+        {{ d.num.toFixed(0) }}
     </div>
 </template>
 <script setup lang="ts">
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+
+const id = ref<any>(null)
+//初始值
+const d = reactive({
+    num: 0
+})
+
 interface Params{
-    value:number
+    value:number,
+    duration:number
 }
 
-withDefaults(defineProps<Params>(),{
-    value:0
+const props = withDefaults(defineProps<Params>(),{
+    value:0,
+    duration:2
 })
+
+const animateToValue = ()=>{
+    gsap.to(d,{
+        scrollTrigger: ".num-" + id.value,
+        duration: props.duration,
+        num:props.value
+
+    })
+}
+
+const asynchronousRendering = ()=>{
+    nextTick(()=>{
+         gsap.registerPlugin(ScrollTrigger);
+        animateToValue();
+    })
+} 
+
+onMounted(()=>{
+    id.value = Math.random().toString(16).slice(2);
+    asynchronousRendering()
+})
+
+watch(() => props.value,()=>{ asynchronousRendering() })
+
+
 </script>
 <style lang="less" scoped>
     .number-card{
