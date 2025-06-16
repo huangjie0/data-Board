8b917fdf8f66cafe13350b0affbfa397ee7a0e76
huangjie0 <1364045672@qq.com>
2025-06-16 15:09:41 +0800

    修改

--- src/views/homeBoard/components/SecondCard.vue
+++ src/views/homeBoard/components/SecondCard.vue
@@ -3,7 +3,7 @@
         <div class="data-f1 data-m-r">
             <TitleCard title="设计单位评分一览" icon-class="icon-faya" color="#1F3696">
                 <template #rightValue>
-                    <el-radio-group v-model="btnValue" size="small" fill="#2471FF" @change="changeRadio">
+                    <el-radio-group v-model="btnValue" size="small" fill="#2471FF" @change="changeRadio1">
                         <el-radio-button :label="item.label" :value="item.value" v-for="(item,index) in options" :key="index"/>
                     </el-radio-group>
                 </template>
@@ -15,9 +15,13 @@
         <div class="data-f1 data-m-l">
             <TitleCard title="项目时长评分" icon-class="icon-muai" color="#1F3696">
                 <template #rightValue>
-                  <div class="data-w-h">
+                    <el-radio-group v-model="btnValue3" size="small" fill="#2471FF" @change="changeRadio3" class="data-m-r">
+                        <el-radio-button :label="item.label" :value="item.value" v-for="(item,index) in options2" :key="index"/>
+                    </el-radio-group>
 
-                  </div>
+                    <el-radio-group v-model="btnValue2" size="small" fill="#2471FF" @change="changeRadio2">
+                        <el-radio-button :label="item.label" :value="item.value" v-for="(item,index) in options" :key="index"/>
+                    </el-radio-group>
                 </template>
                 <div class="data-w-h data-bg data-b-r">
 
@@ -30,6 +34,8 @@
 import Type from '@/views/home/type/index.ts'
 
 const btnValue = ref<any>(Type.designUnit);
+const btnValue2 = ref<any>(Type.designUnit);
+const btnValue3 = ref('0')
 const options = [
     {
         label:'设计单位',
@@ -41,9 +47,25 @@ const options = [
     } 
 ]
 
-const changeRadio = (type:any)=>{
-    btnValue.value = type;
+const options2 = [
+    {
+        label:'在途项目平均时长评分',
+        value:'0'
+    },
+    {
+        label:'可研批复平均时长评分',
+        value:'1'
+    }
+]
+
+const commonFn = (type:any,v:any)=>{
+    v.value = type;
 }
+
+const changeRadio1 = (type:any)=>{ commonFn(type,btnValue) }
+const changeRadio2 = (type:any)=>{ commonFn(type,btnValue2) }
+const changeRadio3 = (type:any)=>{ commonFn(type,btnValue3) }
+
 </script>
 <style lang="less" scoped>
     .second-card{
