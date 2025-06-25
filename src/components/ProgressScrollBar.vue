a14ae3c9e295beb0ca01c9731c9fea9010174ea5
huangjie0 <1364045672@qq.com>
2025-06-25 14:48:58 +0800

    修改

--- src/components/ProgressScrollBar.vue
+++ src/components/ProgressScrollBar.vue
@@ -2,7 +2,7 @@
     <div class="data-between progress-scroll-bar">
         <div class="progress-scroll-bar-text">{{ text }}</div>
         <div class="data-f1">
-            <el-progress color="#407CF8" :percentage="number" :stroke-width="10" striped striped-flow />
+            <el-progress color="#407CF8" :percentage="number" :stroke-width="13" striped striped-flow />
         </div>
     </div>
 </template>
@@ -18,7 +18,7 @@ withDefaults(defineProps<Params>(),{
 </script>
 <style lang="less" scoped>
 .progress-scroll-bar{
-    margin-bottom: 15px;
+    margin-bottom: 1.25rem;
     &-text{
         width: 20%;
         font-size: 12px;
