56830124ff694c3b9ce1b0e2a495332060348264
huangjie0 <1364045672@qq.com>
2025-06-27 09:40:42 +0800

    修改

--- src/components/TaSe.vue
+++ src/components/TaSe.vue
@@ -1,26 +1,78 @@
 <template>
-      <div>
+  <div>
     <!--  动态查询条件   -->
-    <el-form v-if="tableConfig.searchForm" inline ref="form" :model="tableConfig.searchParams">
-      <el-form-item v-for="item in tableConfig.formItem" :key="item.prop" :label="item.label" :prop="item.prop" :rules="item.rules">
+    <el-form
+      v-if="tableConfig.searchForm"
+      inline
+      ref="form"
+      :model="tableConfig.searchParams"
+      :label-width="tableConfig.labelWidth"
+    >
+      <el-form-item
+        v-for="item in tableConfig.formItem"
+        :key="item.prop"
+        :label="item.label"
+        :prop="item.prop"
+        :rules="item.rules"
+      >
         <!-- Input-->
-        <el-input v-if="item.type === 'Input'" v-model.trim="tableConfig.searchParams[item.prop]" :placeholder="item.placeholder" :style="{width: item.width}" :disabled="item.disabled"></el-input>
+        <el-input
+          v-if="item.type === 'Input'"
+          v-model.trim="tableConfig.searchParams[item.prop]"
+          :placeholder="item.placeholder"
+          :style="{ width: item.width }"
+          :disabled="item.disabled"
+        ></el-input>
         <!-- Select-->
-        <el-select filterable v-if="item.type === 'Select'" v-model.trim="tableConfig.searchParams[item.prop]" :placeholder="item.placeholder" :style="{width: item.width}" :disabled="item.disabled">
-          <el-option v-for="selectItem in item.options" :key="selectItem.value || selectItem[item.selectVlaue]" :value="selectItem.value || selectItem[item.selectVlaue]" :label="selectItem.label || selectItem[item.selectLabel]"></el-option>
+        <el-select
+          filterable
+          v-if="item.type === 'Select'"
+          v-model.trim="tableConfig.searchParams[item.prop]"
+          :placeholder="item.placeholder"
+          :style="{ width: item.width }"
+          :disabled="item.disabled"
+        >
+          <el-option
+            v-for="selectItem in item.options"
+            :key="selectItem.value || selectItem[item.selectVlaue]"
+            :value="selectItem.value || selectItem[item.selectVlaue]"
+            :label="selectItem.label || selectItem[item.selectLabel]"
+          ></el-option>
         </el-select>
       </el-form-item>
       <!-- 按钮 -->
       <el-form-item>
-        <el-button :disabled="loadingTable" type="primary" @click="search(tableConfig.searchParams)">查询</el-button>
-        <el-button :disabled="loadingTable" type="warning" @click="reset" v-if="tableConfig.formConfig.resetButton">重置</el-button>
+        <el-button
+          :disabled="loadingTable"
+          type="primary"
+          @click="search(tableConfig.searchParams)"
+          >查询</el-button
+        >
+        <el-button
+          :disabled="loadingTable"
+          type="warning"
+          @click="reset"
+          v-if="tableConfig.formConfig.resetButton"
+          >重置</el-button
+        >
         <template v-for="item in tableConfig.formHandler">
-          <el-button :disabled="loadingTable" v-if="item.element === 'link'" :key="item.key" :type="item.type">
-            <router-link  :to="item.router" >
+          <el-button
+            :disabled="loadingTable"
+            v-if="item.element === 'link'"
+            :key="item.key"
+            :type="item.type"
+          >
+            <router-link :to="item.router">
               <span class="color-white">{{ item.label }}</span>
             </router-link>
           </el-button>
-          <el-button :disabled="loadingTable" v-if="item.element === 'button'" :key="item.key" :type="item.type" @click="item.handler(item)">
+          <el-button
+            :disabled="loadingTable"
+            v-if="item.element === 'button'"
+            :key="item.key"
+            :type="item.type"
+            @click="item.handler(item)"
+          >
             {{ item.label }}
           </el-button>
         </template>
@@ -36,15 +88,26 @@
       :data="tableData"
       border
       fit
-      :height="tableHeight"
+      :height="tableConfig.tableHeight"
       @selection-change="handleSelectionChange"
-      :header-cell-style="{'text-align':'center'}"
-      :cell-style="{'text-align':'center'}"
+      :header-cell-style="{ 'text-align': 'center' }"
+      :cell-style="{ 'text-align': 'center' }"
     >
-      <el-table-column v-if="tableConfig.checkbox" type="selection" width="40"></el-table-column>
+      <el-table-column
+        v-if="tableConfig.checkbox"
+        type="selection"
+        width="40"
+      ></el-table-column>
       <template v-for="item in tableConfig.thead">
         <!--操作 -->
-        <el-table-column align="center" v-if="item.type === 'operation'" :key="item.prop" :prop="item.prop" :label="item.label" :width="item.width">
+        <el-table-column
+          align="center"
+          v-if="item.type === 'operation'"
+          :key="item.prop"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+        >
           <template slot-scope="scope">
             <!--按钮组-->
             <template v-if="item.buttonGroup && item.buttonGroup.length > 0">
@@ -55,23 +118,46 @@
                   :type="button.type"
                   :key="button.id"
                   style="margin: 0 5px"
-                  @click="button.handler(scope.row)" size="small">
+                  @click="button.handler(scope.row)"
+                  size="small"
+                >
                   {{ button.label }}
                 </el-button>
                 <!-- 路由跳转 -->
-                <router-link v-if="button.event === 'link'" :key="button.id" :to="{name: button.name, query: { [button.key]: scope.row[button.value || 'id'] }}" class="mr-10">
-                  <el-button style="margin: 0 5px" :type="button.type" size="small">{{ button.label }}</el-button>
+                <router-link
+                  v-if="button.event === 'link'"
+                  :key="button.id"
+                  :to="{
+                    name: button.name,
+                    query: { [button.key]: scope.row[button.value || 'id'] },
+                  }"
+                  class="mr-10"
+                >
+                  <el-button
+                    style="margin: 0 5px"
+                    :type="button.type"
+                    size="small"
+                    >{{ button.label }}</el-button
+                  >
                 </router-link>
               </template>
             </template>
             <!--额外-->
-            <slot v-if="item.slotName" :name="item.slotName" :data="scope.row"></slot>
+            <slot
+              v-if="item.slotName"
+              :name="item.slotName"
+              :data="scope.row"
+            ></slot>
             <!--删除-->
-<!--            <el-button size="small" v-if="item.default && item.default.deleteButton" :loading="scope.row[item.default.deleteKey || 'id'] == rowId" @click="delConfirm(scope.row[item.default.deleteKey || 'id'])">删除</el-button>-->
+            <!--            <el-button size="small" v-if="item.default && item.default.deleteButton" :loading="scope.row[item.default.deleteKey || 'id'] == rowId" @click="delConfirm(scope.row[item.default.deleteKey || 'id'])">删除</el-button>-->
           </template>
         </el-table-column>
         <!-- 纯文本渲染 -->
-        <el-table-column v-else :prop="item.prop" :label="item.label"></el-table-column>
+        <el-table-column
+          v-else
+          :prop="item.prop"
+          :label="item.label"
+        ></el-table-column>
       </template>
     </el-table>
     <!--  分页  -->
@@ -87,78 +173,75 @@
           :page-sizes="[10, 20, 50, 100]"
           :page-size="10"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="tableData.length">
+          :total="tableData.length"
+        >
         </el-pagination>
       </el-col>
     </el-row>
   </div>
 </template>
 <script setup lang="ts">
-interface Params{
-    tableConfig:any
+interface Params {
+  tableConfig: any
 }
 
-const tableHeight = ref()
-const loadingTable = ref(false)
-const form = ref()
-const table = ref()
+const loadingTable = ref(false);
+const form = ref();
+const table = ref();
 const tableData = ref([
-    { a:'1', b:'281'},
-    { a:'456', b:'281'},
-    { a:'789', b:'281'}
-])
+  { a: "1", b: "281" },
+  { a: "456", b: "281" },
+  { a: "789", b: "281" },
+]);
 
-onMounted(()=>{
-    nextTick(()=>{
-        tableHeight.value = window.innerHeight - 300;
-        // window.onresize = function(){
-        //      tableHeight.value = window.innerHeight - table.value.offsetTop - 170;
-        // }
-    })
-})
+const emits = defineEmits(["search", "handleSelectionChange"]);
 
-const emits = defineEmits(['search','handleSelectionChange'])
+const search = (val: any) => {
+  emits("search", val);
+};
 
-const search = (val:any)=>{
-    emits('search',val)
-}
+const reset = () => {
+  form.value.resetFields();
+};
 
-const reset = ()=>{ form.value.resetFields() }
+const initConfig = () => {
+  loadingTable.value = true;
+  setTimeout(() => {
+    loadingTable.value = false;
+  }, 3000);
+};
+const handleSizeChange = (val: any) => {
+  props.tableConfig.searchParams.pageSize = val;
+  initConfig();
+};
 
-const initConfig = ()=>{
-    loadingTable.value = true
-    setTimeout(()=>{
-        loadingTable.value = false
-    },3000)
-}
-const handleSizeChange = (val:any)=>{
-    props.tableConfig.searchParams.pageSize = val
-    initConfig()
-}
+const handleCurrentChange = (val: any) => {
+  props.tableConfig.searchParams.pageIndex = val;
+  initConfig();
+};
 
-const handleCurrentChange = (val:any) =>{
-    props.tableConfig.searchParams.pageIndex = val
-    initConfig()
-}
+const handleSelectionChange = (val: any) => {
+  emits("handleSelectionChange", val);
+};
 
-const handleSelectionChange = (val:any)=>{
-    emits('handleSelectionChange',val)
-}
+const props = withDefaults(defineProps<Params>(), {});
 
-const props = withDefaults(defineProps<Params>(),{
-
-})
-
-watch(()=>{ props.tableConfig },()=>{ initConfig() },{ immediate:true })
-
+watch(
+  () => {
+    props.tableConfig;
+  },
+  () => {
+    initConfig();
+  },
+  { immediate: true }
+);
 </script>
 <style lang="less" scoped>
-    .padding-top-30{
-        padding-top: 30px;
-    }
+.padding-top-30 {
+  padding-top: 30px;
+}
 
-    .rightClass{
-        float: right;
-    }
-
+.rightClass {
+  float: right;
+}
 </style>
--- src/views/projectOverview/index.vue
+++ src/views/projectOverview/index.vue
@@ -1,35 +1,10 @@
 <template>
-    <TaSe :table-config="tableConfig">
-      <template #content>
-            <div>
-                111
-            </div>
-      </template>
-    </TaSe>
+    <div class="project-overview">
+        111
+    </div>
 </template>
 <script setup lang="ts">
 
-const tableConfig = ref({
-    pagination:true,
-    thead:[
-        { prop: 'a' , label: '发士大夫' },
-        { prop: 'b' , label: '就看见空间' },
-    ],
-    searchForm:{
-      
-    },
-    searchParams:{
-        a:'',
-        b:''
-    },
-    formItem:[
-        { type:'Input',prop:'a',label:'就好',rules:[],width:'200px',disabled:true }
-    ],
-    formConfig:{
-        resetButton:true
-    }
-
-})
 </script>
 <style lang="less" scoped>
 
