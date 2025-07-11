5f45ffe0892b33c7f8df7a41d0c1da5f5a8ef4dd
huangjie0 <1364045672@qq.com>
2025-07-11 11:36:25 +0800

    修改

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -8,12 +8,11 @@
         </template>
         <slot></slot>
         <template #footer>
-            <!-- <div class="dialog-footer">
-                <el-button @click="centerDialogVisible = false">Cancel</el-button>
-                <el-button type="primary" @click="centerDialogVisible = false">
-                Confirm
-                </el-button>
-            </div> -->
+            <div class="data-center">
+                <slot name="buttons"></slot>
+                <el-button type="primary" @click="confirm">{{ confirmTitle }}</el-button>
+                <el-button @click="cancel">{{ cancelTitle }}</el-button>
+            </div>
         </template>
     </el-dialog>
 </template>
@@ -23,17 +22,29 @@ interface Params{
     titleIcon?:string
     color?:string
     dialogWidth?:string
+    confirmTitle?:string
+    cancelTitle?:string
 }
+
+const emits = defineEmits(['confirm'])
 
 const props = withDefaults(defineProps<Params>(),{
     color:'#192C7D',
-    dialogWidth:'800'
+    dialogWidth:'800',
+    confirmTitle:'确定',
+    cancelTitle:'取消'
 })
 const style = computed(()=>{
     return {
         '--color':props.color
     }
 })
+const cancel = ()=>{
+    dialogVisible.value = false
+}
+const confirm = ()=>{
+    emits('confirm')
+}
 const dialogVisible = ref(false)
 const open = ()=>{
     dialogVisible.value = true
@@ -41,7 +52,6 @@ const open = ()=>{
 defineExpose({
     open
 })
-
 </script>
 <style lang="less" scoped>
 .icon-title{
