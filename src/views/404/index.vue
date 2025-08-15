ccec4f3512e9574c941aa2be6436ca577038b10e
huangjie4 <1364045672@qq.com>
2025-08-15 16:20:48 +0800

    修改

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,6 +1,9 @@
 <template>
      <div class="data-w-h data-center">
           <el-empty :image-size="200" description="访问的页面不存在！"/>
+          <div>
+               1111
+          </div>
      </div>
 </template>
 <script setup lang="ts">
